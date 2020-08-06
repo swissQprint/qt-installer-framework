12052ef69c90f2d96c2429b44f5d7346c1749607
Kevin Bischofberger swissQprint <kbi@swissqprint.com>
2020-08-06 14:32:18 +0200

    Anpassung der Texte / Links auf der Willkommensseite.

--- src/libs/installer/sqp/WelcomePage.cpp
+++ src/libs/installer/sqp/WelcomePage.cpp
@@ -1,4 +1,5 @@
 #include "sqp/WelcomePage.hpp"
+#include "constants.h"
 #include <QDebug>
 
 namespace QInstaller {
@@ -9,8 +10,18 @@ WelcomePage::WelcomePage(PackageManagerCore *core)
 {
     ui.setupUi(this);
     setObjectName(QLatin1String("WelcomePage"));
-    setColoredTitle(tr("swissQprint Software Studio"));
+    ui.lblMessage->setObjectName(QLatin1String("MessageLabel"));
+    ui.lblSqp->setObjectName(QLatin1String("SwissQprintLinkLabel"));
+    ui.lblExtranet->setObjectName(QLatin1String("SwissQprintExtranetLinkLabel"));
+
+    const auto t = tr("Welcome to %1").arg(core->value(scTitle));
+    setColoredTitle(t);
     setSettingsButtonRequested(true);
+    QString message = tr("This maintenance tool can be used to add, update or remove swissQprint software components.");
+    if (core->isInstaller()) {
+        message = tr("This installer provides a range of swissQprint software components.");
+    }
+    ui.lblMessage->setText(message);
 }
 
 WelcomePage::~WelcomePage() {  }
